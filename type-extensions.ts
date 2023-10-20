56c50260cb9435e0b4058ae6ac97f42765c8ce3b
eum602 <eum602@gmail.com>
2023-10-20 18:32:55 -0500

    chore: add base contract files
    Signed-off-by: eum602 <eum602@gmail.com>

---
+++ type-extensions.ts
@@ -0,0 +1,37 @@
+import "hardhat/types/runtime";
+import "hardhat/types/config";
+import { Contract, ContractFactory } from "ethers";
+import { GasModelProvider, GasModelSigner } from "@lacchain/gas-model-provider";
+import { GasModelSignerModified } from "./GasModelModified";
+
+declare module "hardhat/types/config" {
+  interface HttpNetworkUserConfig {
+    nodeAddress: string;
+    expiration: number;
+    chainId?: number;
+    from?: string;
+    gas?: "auto" | number;
+    gasPrice?: "auto" | number;
+    gasMultiplier?: number;
+    url?: string;
+    timeout?: number;
+    httpHeaders?: {
+      [name: string]: string;
+    };
+    privateKeys?: string[];
+  }
+}
+
+declare module "hardhat/types/runtime" {
+  interface HardhatRuntimeEnvironment {
+    lacchain: {
+      baseRelayAddress: string;
+      provider: GasModelProvider;
+      deployContract: (
+        contract: ContractFactory,
+        ...params: any
+      ) => Promise<Contract>;
+      getSigners: () => GasModelSignerModified[];
+    };
+  }
+}
